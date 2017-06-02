313c51cc4d92cc78fa1f0a4cfb27a990538e46f3
Dave <wm.david.gallup@gmail.com>
2017-06-01 17:15:31 -0700

    Handle requests for path with index values

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,38 @@
 const express = require('express');
-const pets = require('./pets');
+const path    = require('path');
+const fs      = require('fs');
 
-const app = express();
+const app     = express();
+const port    = 8000;
 
-app.get('/pets', handleGetPetsRoot);
+app.get('/pets', (req, res)=>{
+  fs.readFile(`${__dirname}/pets.json`, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
 
-app.listen(8000, ()=>{console.log('Listening on port 8000');});
+    res.send(JSON.parse(petsJSON));
+  });
+});
 
-function handleGetPetsRoot(req, res){
-  
-}
+app.get('/pets/:index', (req, res)=>{
+  fs.readFile(`${__dirname}/pets.json`, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    var pets = JSON.parse(petsJSON)
+
+    if(req.params.index < 0 || req.params.index > pets.length -1){
+      res.status(404);
+      res.setHeader('content-type', 'text/plain');
+      res.send('Not Found')
+    }
+    res.send(pets[req.params.index]);
+  });
+});
+
+app.listen(port, ()=>{console.log('Listening on port', port);});
+
+
